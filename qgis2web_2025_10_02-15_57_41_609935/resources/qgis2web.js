64ef927ee1fdbbe7a24ebc2c3e7c35447c979778
rexsim-apen <rex@apen4ej.org>
2025-10-02 16:02:50 -0700

    Districts, Terminals, Refineries

--- qgis2web_2025_10_02-15_57_41_609935/resources/qgis2web.js
+++ qgis2web_2025_10_02-15_57_41_609935/resources/qgis2web.js
@@ -469,6 +469,23 @@ var bottomRightContainerDiv = document.getElementById('bottom-right-container')
 
 //layerswitcher
 
+var layerSwitcher = new ol.control.LayerSwitcher({
+    activationMode: 'click',
+	startActive: true,
+	tipLabel: "Layers",
+    target: 'top-right-container',
+	collapseLabel: '»',
+	collapseTipLabel: 'Close'
+    });
+map.addControl(layerSwitcher);
+if (hasTouchScreen || isSmallScreen) {
+	document.addEventListener('DOMContentLoaded', function() {
+		setTimeout(function() {
+			layerSwitcher.hidePanel();
+		}, 500);
+	});	
+}
+
 
 
 
